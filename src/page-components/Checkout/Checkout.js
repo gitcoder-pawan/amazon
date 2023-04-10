00e1e6d79ff9f06e3c425636db9e2c61e2b96fd6
pawan23 <pawan11072000@gmail.com>
2023-04-10 17:29:36 +0530

    added stepper tab in checkout page

--- src/page-components/Checkout/Checkout.js
+++ src/page-components/Checkout/Checkout.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cart from './Cart/Cart';
+import Address from './Address/Address';
+import PaymentSection from './PaymentSection/PaymentSection';
 import './checkout.css';
 const Checkout = () => {
+  const [step, setStep] = useState(0);
+  const handleStep = (index)=>{
+    setStep(index);
+  }
+  const menuMappings = {
+    0:<Cart/>,
+    1:<Address/>,
+    2: <PaymentSection/>,
+  }
   return (
-    <div>Checkout</div>
+    <div className="checkout">
+      <div className="checkout__heading">checkout page</div>
+      <div className="checkout__stepper">
+        {['Cart', 'Address', 'Payment Section'].map((itm, index)=>{
+          return <div className={`checkout__stepperTab ${index===step ? 'checkout__stepperTabActive': ''}`} onClick={()=>{handleStep(index)}}>{itm}</div>
+        })}
+      </div>
+      <div className="checkout__menu">
+        {menuMappings[step]}
+      </div>
+    </div>
   )
 }
 
